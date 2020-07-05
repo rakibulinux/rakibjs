2ef9176b7b11a1cc0d90958697d768da1c6ef01d
Rakibul Islam <rakibulinux@gmail.com>
2020-07-05 09:43:08 +0000

    first commit

---
+++ dataType.js
@@ -0,0 +1,23 @@
+var i;
+var fruits=["apple","grapes"];
+var newFruits=fruits;
+newFruits [0]="painaple";
+console.log("the value of New",newFruits)
+console.log("the value of New",fruits)
+
+const obj1={
+    empID:101,empName:"rakib"
+};
+obj1.empID=999;
+console.log("Const obj value=" , obj1);
+i=10;
+
+var obj={
+    "empID":101,
+    "empName":"",
+    "salary":4
+};
+console.log("Wmploy Id" +obj.empID);
+console.log("rakib testlog"+obj.salary);
+console.log("rakib testlog",obj);
+console.log("rakib testlog"+obj);
