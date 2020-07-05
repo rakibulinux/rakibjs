48ffe54d85e38aa861437571b8683ccb4cffb876
Rakibul Islam <rakibulinux@gmail.com>
2020-07-05 13:23:44 +0000

    my 3rd commit on js

--- dataType.js
+++ dataType.js
@@ -1,10 +1,6 @@
 var i;
-var fruits=["apple","grapes"];
-var newFruits=fruits;
-newFruits [0]="painaple";
-console.log("the value of New",newFruits)
-console.log("the value of New",fruits)
-
+i=[1,2,3,7,8]
+console.log("value of the array are",i)
 const obj1={
     empID:101,empName:"rakib"
 };
@@ -14,10 +10,25 @@ i=10;
 
 var obj={
     "empID":101,
-    "empName":"",
-    "salary":4
+    "empName":"Rakibulinux",
+    "salary":46355.89
 };
 console.log("Wmploy Id" +obj.empID);
 console.log("rakib testlog"+obj.salary);
 console.log("rakib testlog",obj);
 console.log("rakib testlog"+obj);
+var fruits=["apple","grapes"];
+var newFruits=fruits;
+newFruits [0]="painaple";
+console.log("the value of New",newFruits)
+console.log("the value of New",fruits)
+
+obj.empID=999;
+
+console.log("Employee id: "+obj.empID);
+
+obj.deptID="D1";
+console.log("Depertment id: ",obj.deptID);
+
+obj.empName="rakib";
+console.log("Employee name is "+obj.empName);
